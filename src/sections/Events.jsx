36f29027738df5495bd699b2361ae306b7724499
NeverDh <63487149+NeverDh@users.noreply.github.com>
2024-08-04 01:41:00 -0300

    Eventos estilizado

--- src/sections/Events.jsx
+++ src/sections/Events.jsx
@@ -2,38 +2,43 @@ import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import './Events.css';
 
+import Gojo from './gojo.jpg'
+
 const Events = () => {
   return (
     <Container fluid className="event-section">
-      <Row>
-        <Col md={6}>
+      <Col className='first-custom-column'>
+       <Col md={6} className='first-column-events'>
+        <div className='fernando-filho-da-puta'>
           <h1 className="event-title">Teleprompter Carioca no G20</h1>
           <p className="event-description">
             Estamos no G20! Nossos equipamentos são essenciais para os discursos de líderes mundiais. Com nosso suporte, garantimos apresentações impecáveis em um evento de proporção global.
           </p>
-        </Col>
-        <Col md={6}>
-          <img src="/event-image.png" alt="G20 Event" className="event-image" />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
+        </div>
           <Card className="event-card">
-            <ul>
-              <li><strong>Teleprompter</strong> - Com monitor de retorno</li>
-              <li><strong>Teleprompter</strong> - DSLR 10 polegadas</li>
-            </ul>
+            <div className="custom-list">
+              <ul>
+                <li><strong>Ipad</strong></li>
+                  <ul className='subitem-list'><li>logarina</li></ul>
+                <li><strong>Teleprompter</strong></li>
+                  <ul className='subitem-list'><li>câmera portátil</li></ul>
+              </ul>
+              <ul>
+                <li><strong>Ipad</strong></li>
+                  <ul className='subitem-list'><li>logarina</li></ul>
+                <li><strong>Teleprompter</strong></li>
+                <ul className='subitem-list'><li>câmera portátil</li></ul>
+              </ul>
+            </div>
           </Card>
         </Col>
-        <Col md={6}>
-          <Card className="event-card">
-            <ul>
-              <li><strong>Ipad</strong> - logarina</li>
-              <li><strong>Teleprompter</strong> - câmera portátil</li>
-            </ul>
-          </Card>
+        
+        <Col md={6} className='custom-event'>
+          <img src={Gojo} alt="G20 Event" className="event-image" />
         </Col>
-      </Row>
+        
+      </Col>
+
     </Container>
   );
 };
